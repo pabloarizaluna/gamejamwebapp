1426bcd53feaa5f37542936310085d230f9fdda5
pabloarizaluna <pabloarizaluna@gmail.com>
2015-10-25 08:34:52 -0500

    correcciones

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ActiveRecord::Base
   has_many :tutors
   has_many :affinities
   validates :name, presence: true
-  validates :description, presence: true, :length{maximum: 20}
+  validates :description, presence: true, length: {maximum: 20}
   validates :type, presence: true
   validates :photo, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,56 +14,56 @@
 ActiveRecord::Schema.define(version: 20151024211537) do
 
   create_table "affinities", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "courses", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name"
     t.datetime "end_date"
-    t.text     "description", limit: 65535
-    t.string   "type",        limit: 255
-    t.binary   "photo",       limit: 65535
+    t.text     "description"
+    t.string   "type"
+    t.binary   "photo"
     t.boolean  "finished"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "news", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.text     "description", limit: 65535
-    t.string   "photo",       limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "title"
+    t.text     "description"
+    t.string   "photo"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "rewards", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.text     "description", limit: 65535
-    t.binary   "photo",       limit: 65535
-    t.integer  "cost",        limit: 4
-    t.string   "type",        limit: 255
-    t.integer  "quantity",    limit: 4
+    t.string   "name"
+    t.text     "description"
+    t.binary   "photo"
+    t.integer  "cost"
+    t.string   "type"
+    t.integer  "quantity"
     t.boolean  "available"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "type",            limit: 255
-    t.string   "name",            limit: 255
-    t.string   "last_name",       limit: 255
-    t.string   "user_name",       limit: 255
-    t.string   "institution",     limit: 255
-    t.integer  "age",             limit: 4
-    t.text     "description",     limit: 65535
-    t.string   "email",           limit: 255
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.string   "password_digest", limit: 255
+    t.string   "type"
+    t.string   "name"
+    t.string   "last_name"
+    t.string   "user_name"
+    t.string   "institution"
+    t.integer  "age"
+    t.text     "description"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
