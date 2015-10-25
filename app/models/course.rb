a22dad0ae3ba86e295da866d3d7f53e2cef421cd
pabloarizaluna <pabloarizaluna@gmail.com>
2015-10-24 22:41:53 -0500

    seventh commit

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,18 @@ class Course < ActiveRecord::Base
   has_many :tutors
   has_many :affinities
   validates :name, presence: true
-  validates :description, presence: true
+  validates :description, presence: true, :length{maximum: 20}
   validates :type, presence: true
   validates :photo, presence: true
+
+  def findcoursetime
+    @newcourse = []
+    @New = New.all
+    t=Time.now.beginning_of_day
+    @New each do |new|
+      if course.created_at>t
+          @newcourse << course
+      end
+      return @newcourse
+  end
 end
